14c13616d8b8f747c5bc47e47a441f90e96c7cbc
JU1111111 <115618396+JU1111111@users.noreply.github.com>
2023-07-27 00:52:56 +0200

    yeeeeeeeeeeeeeet

---
+++ deezNuts.py
@@ -0,0 +1,55 @@
+from moviepy.editor  import *
+import math
+
+
+class ChrisNolan:
+
+    vidSize = (1080,1920)
+    def __init__(self, targetTime = 20, fps = 24):
+        self.targetTime = targetTime
+        self.fpsRussia = fps
+        self.audioPath = "schpoddyfei\\gg.mp3"
+        self.vidPath = "vidCon\\test.mp4"
+
+    def yeetInToAVid(self, images):
+        clips = []
+        targetMaxTime = 20
+        numberOfImages = len(images)
+        individualLen = math.floor(self.targetMaxTime / numberOfImages)
+        t = 0
+        actualMaxTime = numberOfImages * individualLen
+        daSize = self.vidSize
+        colorthing = ColorClip(size=daSize, color=(0,0,0), ismask=False).set_start(0).set_duration(actualMaxTime).set_pos(("center","center"))
+        clips.append(colorthing)
+
+        for i in range(numberOfImages):
+            theImage = images[i]
+            clip = ImageClip(theImage).set_start(t).set_duration(individualLen).set_pos(("center","center")).resize(width=self.vidSize[0])
+            clips.append(clip)
+            t += individualLen
+
+
+        final = CompositeVideoClip(clips)
+
+        final.write_videofile(self.vidPath, fps=self.fpsRussia)
+
+        videoclip = VideoFileClip(self.vidPath)
+        audioFileThing = AudioFileClip(self.audiopath).set_duration(actualMaxTime)
+        audioclip = CompositeAudioClip([audioFileThing])
+        videoclip.audio = audioclip
+        yeet = self.vidPath.split(".")
+        name = yeet[-2]
+        name += "_withAudio"
+        audvidPath = name + yeet[-1]
+        videoclip.write_videofile(audvidPath, fps=self.fpsRussia)
+
+
+    def testDaShit(self):
+        imagineallthepeople = os.listdir("imagineallthepeople\\")
+
+        lesImages = []
+
+        for image in imagineallthepeople:
+            lesImages.append("imagineallthepeople\\" + image)
+
+        self.yeetInToAVid(lesImages)
